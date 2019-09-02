adff5093f4e077b689d67cbcb4fdab058da07877
Bryan Villatoro <bvillatoro@ymail.com>
2019-09-02 10:28:52 -0400

    setup logging

--- src/main/java/com/smoothstack/borrower/borrower_micro_service/controller/BorrowerController.java
+++ src/main/java/com/smoothstack/borrower/borrower_micro_service/controller/BorrowerController.java
@@ -1,11 +1,13 @@
 package com.smoothstack.borrower.borrower_micro_service.controller;
 
 import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,16 +25,22 @@ import com.smoothstack.borrower.borrower_micro_service.service.BorrowerService;
 @RequestMapping(path = "/api/borrower")
 public class BorrowerController {
 
+	Logger logger = LoggerFactory.getLogger(BorrowerController.class);
+	
 	@Autowired
 	private BorrowerService borrowerService;
+	
+	
 
 	@RequestMapping(value = "/bookloans", method = RequestMethod.GET, produces = { "application/xml",
 			"application/json" })
 	public ResponseEntity<List<BookLoan>> allLoans() {
 		List<BookLoan> bookLoan = borrowerService.getAllLoans();
 		if (bookLoan.isEmpty()) {
+			logger.info("Http status bad");
 			return new ResponseEntity<List<BookLoan>>(HttpStatus.NOT_FOUND);
 		} else {
+			logger.info("Http status good");
 			return new ResponseEntity<List<BookLoan>>(bookLoan, HttpStatus.OK);
 		}
 	}
@@ -59,9 +67,11 @@ public class BorrowerController {
 		} else if (borrowerService.validNumberOfCopies(bookLoan)) {
 			bookLoan = borrowerService.checkOutBook(bookLoan);
 
+			logger.info("Book checked out "+ bookLoan);
 			return new ResponseEntity<BookLoan>(bookLoan, HttpStatus.CREATED);
 
 		} else {
+			logger.info("Book failed to be checked out");
 			return new ResponseEntity<BookLoan>(HttpStatus.FAILED_DEPENDENCY);
 		}
 	}
@@ -79,8 +89,10 @@ public class BorrowerController {
 		boolean checkedOut = borrowerService.bookCheckedOutAlready(bookLoan);
 		if (checkedOut) {
 			borrowerService.deleteBookLoans(bookLoan);
+			logger.info("Book Loan Returned bookid: "+bookId+" branchid: "+branchId+" cardno: "+cardNumber);
 			return HttpStatus.OK;
 		} else {
+			logger.info("Failed to return book");
 			return HttpStatus.NOT_FOUND;
 		}
 	}
